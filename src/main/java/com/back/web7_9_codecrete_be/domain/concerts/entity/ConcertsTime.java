0c660cba2a00608be4225ebc6d99e63d3171b773
Creamcheesepie <gnldbs1004@naver.com>
2025-12-10 14:07:24 +0900

    feat: 엔티티 클래스 명 수정

--- src/main/java/com/back/web7_9_codecrete_be/domain/concerts/entity/ConcertsTime.java
+++ src/main/java/com/back/web7_9_codecrete_be/domain/concerts/entity/ConcertsTime.java
@@ -7,21 +7,15 @@ import java.time.LocalDateTime;
 
 @Entity
 @Getter
-public class ConcertsReviewEntity {
+public class ConcertsTime {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne
-    private ConcertsEntity concertsEntity;
+    private Concerts concerts;
 
-    // TODO : 연관 객체 이어주기
+    private LocalDateTime startTime;
 
-    private String title;
-
-    private String content;
-
-    private LocalDateTime createdTime;
-
-    private LocalDateTime modifiedTime;
+    private LocalDateTime endTime;
 }
--- src/main/java/com/back/web7_9_codecrete_be/domain/concerts/repository/ConcertsLikeRepository.java
+++ src/main/java/com/back/web7_9_codecrete_be/domain/concerts/repository/ConcertsLikeRepository.java
@@ -1,10 +1,10 @@
 package com.back.web7_9_codecrete_be.domain.concerts.repository;
 
-import com.back.web7_9_codecrete_be.domain.concerts.entity.ConcertsLikeEntity;
+import com.back.web7_9_codecrete_be.domain.concerts.entity.ConcertsLike;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ConcertsLikeRepository extends JpaRepository<ConcertsLikeEntity, Long> {
+public interface ConcertsLikeRepository extends JpaRepository<ConcertsLike, Long> {
 }
 
