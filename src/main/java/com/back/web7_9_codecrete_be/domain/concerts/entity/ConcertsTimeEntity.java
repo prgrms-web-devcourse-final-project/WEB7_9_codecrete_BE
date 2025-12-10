1f65f5899b35065bb92e4cc2e465ebd49fe96874
Creamcheesepie <gnldbs1004@naver.com>
2025-12-10 11:48:08 +0900

    feat: 엔티티에 @Getter 어노테이션 추가

--- src/main/java/com/back/web7_9_codecrete_be/domain/concerts/entity/ConcertsTimeEntity.java
+++ src/main/java/com/back/web7_9_codecrete_be/domain/concerts/entity/ConcertsTimeEntity.java
@@ -1,10 +1,12 @@
 package com.back.web7_9_codecrete_be.domain.concerts.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Getter
 public class ConcertsTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
