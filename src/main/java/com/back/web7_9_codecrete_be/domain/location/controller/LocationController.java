b9154577e44c2907837b7009b950ecacec44ba22
tobbot16 <tobbot16@naver.com>
2025-12-10 15:04:16 +0900

    service, controller 의존성 주입

--- src/main/java/com/back/web7_9_codecrete_be/domain/location/controller/LocationController.java
+++ src/main/java/com/back/web7_9_codecrete_be/domain/location/controller/LocationController.java
@@ -1,4 +1,15 @@
 package com.back.web7_9_codecrete_be.domain.location.controller;
 
+import com.back.web7_9_codecrete_be.domain.location.service.LocationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/location")
+@RequiredArgsConstructor
 public class LocationController {
+
+    private final LocationService locationService;
+
 }
--- src/main/java/com/back/web7_9_codecrete_be/domain/location/service/LocationService.java
+++ src/main/java/com/back/web7_9_codecrete_be/domain/location/service/LocationService.java
@@ -1,4 +1,13 @@
 package com.back.web7_9_codecrete_be.domain.location.service;
 
+import com.back.web7_9_codecrete_be.domain.location.repository.LocationRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class LocationService {
+
+    private final LocationRepository locationRepository;
+
 }
