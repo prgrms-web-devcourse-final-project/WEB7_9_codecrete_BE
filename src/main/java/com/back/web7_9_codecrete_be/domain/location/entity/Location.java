891c05014fe0e94a83b0cc573a7c24527210230f
tobbot16 <tobbot16@naver.com>
2025-12-10 12:45:36 +0900

    feat: entity 클래스 생성

--- src/main/java/com/back/web7_9_codecrete_be/domain/location/entity/Location.java
+++ src/main/java/com/back/web7_9_codecrete_be/domain/location/entity/Location.java
@@ -1,4 +1,34 @@
 package com.back.web7_9_codecrete_be.domain.location.entity;
 
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="location")
+@Getter
+@NoArgsConstructor
 public class Location {
+
+    @Id
+    @Column(nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name="user_id", nullable = false)
+    private User user;
+
+    @Column(length = 255)
+    private String address;
+
+    @Column(nullable = false)
+    private LocalDateTime modified_date;
+
+    private double lat;
+
+    private double lon;
 }
