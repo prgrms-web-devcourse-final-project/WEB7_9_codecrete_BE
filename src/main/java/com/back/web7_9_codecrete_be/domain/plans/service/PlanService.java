7b0031986e27b8e7eab98567802d7b315f2978c0
kms152000 <kms152000@naver.com>
2025-12-10 15:05:37 +0900

    feat: 플랜 도메인 기본 MVC 구조 및 엔티티 추가

---
+++ src/main/java/com/back/web7_9_codecrete_be/domain/plans/service/PlanService.java
@@ -0,0 +1,24 @@
+package com.back.web7_9_codecrete_be.domain.plans.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class PlanService {
+    // 계획 목록 조회
+    // 계획 상세 조회
+    // 계획 생성
+    // 계획 수정
+    // 계획 삭제
+    // 계획 공유 초대
+    // 계획 공유 수락
+    // 계획 공유 거절
+    // 계획 공유 인원 추방
+    // 계획 공유 나가기
+}
+
