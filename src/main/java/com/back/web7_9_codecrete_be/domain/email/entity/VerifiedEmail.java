f88719a6c7b7d6a271ea927345b5917e8f06862b
larama-C <ccbj0825@naver.com>
2025-12-11 12:50:50 +0900

    feat: 이메일 인증 코드 및 검증 도메인 구현

---
+++ src/main/java/com/back/web7_9_codecrete_be/domain/email/entity/VerifiedEmail.java
@@ -0,0 +1,17 @@
+package com.back.web7_9_codecrete_be.domain.email.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class VerifiedEmail {
+    @Id
+    private String email;
+}
