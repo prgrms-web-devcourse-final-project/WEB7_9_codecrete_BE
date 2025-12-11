5980030954b9c1c1c7c2484e4ce1c09a4ed645a8
heygeeji <heygeeji@gmail.com>
2025-12-11 10:42:21 +0900

    feat: Concert 연관관계 설정

--- src/main/java/com/back/web7_9_codecrete_be/domain/chats/entity/ChatRoom.java
+++ src/main/java/com/back/web7_9_codecrete_be/domain/chats/entity/ChatRoom.java
@@ -2,11 +2,16 @@ package com.back.web7_9_codecrete_be.domain.chats.entity;
 
 import java.time.LocalDateTime;
 
+import com.back.web7_9_codecrete_be.domain.concerts.entity.Concert;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import lombok.Getter;
 
 @Getter
@@ -23,5 +28,9 @@ public class ChatRoom {
 
 	@Column(nullable = false)
 	private LocalDateTime expiresDate;
+
+	@OneToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "concert_id", nullable = false, unique = true)
+	private Concert concert;
 
 }
