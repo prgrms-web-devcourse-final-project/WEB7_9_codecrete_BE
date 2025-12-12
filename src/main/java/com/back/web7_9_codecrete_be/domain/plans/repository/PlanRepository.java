68614805877d42edc5c06f58fe93268111a8bf06
kms152000 <kms152000@naver.com>
2025-12-12 09:19:08 +0900

    refactor: 엔티티 및 DTO 구조 수정

--- src/main/java/com/back/web7_9_codecrete_be/domain/plans/repository/PlanRepository.java
+++ src/main/java/com/back/web7_9_codecrete_be/domain/plans/repository/PlanRepository.java
@@ -1,11 +1,29 @@
 package com.back.web7_9_codecrete_be.domain.plans.repository;
 
 import com.back.web7_9_codecrete_be.domain.plans.entity.Plan;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
 
 @Repository
 public interface PlanRepository extends JpaRepository<Plan, Long> {
 
-}
+    /**
+     * @EntityGraph 사용 시:
+     * - Plan + participants + routes를 LEFT OUTER JOIN으로 한 번에 조회.
+     * - 총 1번의 쿼리만 실행되어 N + 1 문제 방지 & 성능 향상.
+     */
+    @EntityGraph(attributePaths = {"participants", "routes"})
+    Optional<Plan> findById(Long id);
 
+    /**
+     * @param userId
+     * @return userId가 참가자로 연결된 모든 Plan을 조회
+     */
+    @EntityGraph(attributePaths = {"participants"})
+    List<Plan> findDistinctByParticipants_UserId(Long userId);
+}
