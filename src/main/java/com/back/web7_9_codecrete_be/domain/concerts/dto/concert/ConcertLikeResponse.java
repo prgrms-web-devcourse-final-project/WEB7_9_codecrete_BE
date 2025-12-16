25244703e69b32952c8d4cda5ddd7b2981fbc9b9
Creamcheesepie <gnldbs1004@naver.com>
2025-12-16 10:37:16 +0900

    feat: 스웨거 문서화 작업

--- src/main/java/com/back/web7_9_codecrete_be/domain/concerts/dto/concert/ConcertLikeResponse.java
+++ src/main/java/com/back/web7_9_codecrete_be/domain/concerts/dto/concert/ConcertLikeResponse.java
@@ -2,11 +2,15 @@ package com.back.web7_9_codecrete_be.domain.concerts.dto.concert;
 
 import com.back.web7_9_codecrete_be.domain.concerts.entity.Concert;
 import com.back.web7_9_codecrete_be.domain.concerts.entity.ConcertLike;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 @Getter
 public class ConcertLikeResponse {
+    @Schema(description = "공연 ID 입니다.")
     private Long concertId;
+
+    @Schema(description = "공연 좋아요 여부입니다.")
     private Boolean isLike;
 
     public ConcertLikeResponse(Concert concert, Boolean isLike) {
