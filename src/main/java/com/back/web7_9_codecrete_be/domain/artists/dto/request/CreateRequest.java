7f47452bc9c598b23384a3ecc793b09c207eb7f8
Jung Yunseo <yunseo2488@naver.com>
2025-12-16 11:07:52 +0900

    refactor : DTO 스키마 추가

--- src/main/java/com/back/web7_9_codecrete_be/domain/artists/dto/request/CreateRequest.java
+++ src/main/java/com/back/web7_9_codecrete_be/domain/artists/dto/request/CreateRequest.java
@@ -1,9 +1,21 @@
 package com.back.web7_9_codecrete_be.domain.artists.dto.request;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public record CreateRequest(
+        @NotNull(message = "아티스트 이름은 필수로 입력해야합니다.")
+        @Size(max = 200, message = "아티스트 이름은 200자를 넘길 수 없습니다.")
         String artistName,
+
+        @Size(max = 150, message = "아티스트 그룹 이름은 150자를 넘길 수 없습니다.")
         String artistGroup,
+
+        @NotNull(message = "아티스트 타입은 필수로 입력해야합니다(SOLO or GROUP)")
         String artistType,
+
+        @NotNull(message = "장르는 필수로 입력해야합니다.")
+        @Size(max = 30, message = "장르는 30자를 넘길 수 없습니다.")
         String genreName
 ) {
 }
--- src/main/java/com/back/web7_9_codecrete_be/domain/artists/dto/request/UpdateRequest.java
+++ src/main/java/com/back/web7_9_codecrete_be/domain/artists/dto/request/UpdateRequest.java
@@ -1,9 +1,17 @@
 package com.back.web7_9_codecrete_be.domain.artists.dto.request;
 
+import jakarta.validation.constraints.Size;
+
 public record UpdateRequest(
+        @Size(max = 200, message = "아티스트 이름은 200자를 넘길 수 없습니다.")
         String artistName,
+
+        @Size(max = 150, message = "아티스트 그룹 이름은 150자를 넘길 수 없습니다.")
         String artistGroup,
+
         String artistType,
+
+        @Size(max = 30, message = "장르 이름은 30자를 넘길 수 없습니다.")
         String genreName
 ) {
 }
