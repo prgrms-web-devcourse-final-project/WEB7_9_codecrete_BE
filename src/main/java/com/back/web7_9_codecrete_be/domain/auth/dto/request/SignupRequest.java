4afe5a2491ce0a73fde720b39c619d7373113ed8
larama-C <ccbj0825@naver.com>
2025-12-17 15:28:46 +0900

    refactor: 이메일 Validation 정규식 기반으로 변경

--- src/main/java/com/back/web7_9_codecrete_be/domain/auth/dto/request/SignupRequest.java
+++ src/main/java/com/back/web7_9_codecrete_be/domain/auth/dto/request/SignupRequest.java
@@ -1,7 +1,6 @@
 package com.back.web7_9_codecrete_be.domain.auth.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
@@ -11,7 +10,10 @@ import lombok.Getter;
 public class SignupRequest {
 
     @NotBlank(message = "이메일은 필수입니다.")
-    @Email(message = "이메일 형식이 올바르지 않습니다.")
+    @Pattern(
+            regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$",
+            message = "이메일 형식이 올바르지 않습니다."
+    )
     @Schema(description = "사용자 이메일", example = "test@example.com")
     private String email;
 
