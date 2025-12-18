f645a7923ddd8042446110b70dec7643094adc60
heygeeji <heygeeji@gmail.com>
2025-12-18 09:42:53 +0900

    fix: 채팅방 활성화 기간을 날짜 기준으로 수정

--- src/main/java/com/back/web7_9_codecrete_be/domain/chats/service/ChatPolicyService.java
+++ src/main/java/com/back/web7_9_codecrete_be/domain/chats/service/ChatPolicyService.java
@@ -2,6 +2,7 @@ package com.back.web7_9_codecrete_be.domain.chats.service;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
@@ -43,21 +44,27 @@ public class ChatPolicyService {
 
 		// 정책 기간 계산
 		LocalDateTime now = LocalDateTime.now();
+
+		LocalDateTime policyStart = ticketTime.toLocalDate()
+			.minusDays(3)
+			.atStartOfDay();
+
+		LocalDateTime policyEnd = ticketTime.toLocalDate()
+			.plusDays(3)
+			.atTime(LocalTime.MAX);
+
 		boolean available =
-			now.isAfter(ticketTime.minusDays(3)) &&
-				now.isBefore(ticketTime.plusDays(3));
+			now.isAfter(policyStart) &&
+				now.isBefore(policyEnd);
 
 		// TTL 계산 후 캐싱
-		Duration ttl = calculateTtl(now, ticketTime);
+		Duration ttl = calculateTtl(now, policyStart, policyEnd);
 		redisTemplate.opsForValue().set(key, available, ttl);
 
 		return available;
 	}
 
-	private Duration calculateTtl(LocalDateTime now, LocalDateTime ticketTime) {
-
-		LocalDateTime policyStart = ticketTime.minusDays(3);
-		LocalDateTime policyEnd   = ticketTime.plusDays(3);
+	private Duration calculateTtl(LocalDateTime now, LocalDateTime policyStart, LocalDateTime policyEnd) {
 
 		// 아직 정책 시작 전
 		if (now.isBefore(policyStart)) {
