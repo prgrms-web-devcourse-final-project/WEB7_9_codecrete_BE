9f5e6349cc87a8b1cd46c6d43c0c83a6d9f5f9a9
kms152000 <kms152000@naver.com>
2025-12-22 16:25:07 +0900

    feat: PlanErrorCode 추가

--- src/main/java/com/back/web7_9_codecrete_be/global/error/code/PlanErrorCode.java
+++ src/main/java/com/back/web7_9_codecrete_be/global/error/code/PlanErrorCode.java
@@ -15,9 +15,13 @@ public enum PlanErrorCode implements ErrorCode {
 	PLAN_UNAUTHORIZED(HttpStatus.FORBIDDEN, "P-103", "해당 작업을 수행할 권한이 없습니다."),
 	SCHEDULE_INVALID_TRANSPORT_FIELDS(HttpStatus.BAD_REQUEST, "P-104", "교통 수단 타입일 경우 출발지/도착지 좌표, 거리, 교통 수단 종류는 필수입니다."),
 	SCHEDULE_INVALID_LOCATION_FOR_TRANSPORT(HttpStatus.BAD_REQUEST, "P-105", "교통 수단 타입일 경우 locationLat/Lon은 사용할 수 없습니다. endPlaceLat/Lon을 사용해주세요."),
-	SCHEDULE_INVALID_LOCATION_COORDINATES(HttpStatus.BAD_REQUEST, "P-107", "위도와 경도는 함께 제공되어야 합니다."),
 	CONCERT_NOT_FOUND(HttpStatus.NOT_FOUND, "P-106", "공연을 찾을 수 없습니다."),
-	SCHEDULE_MAIN_EVENT_NOT_DELETABLE(HttpStatus.FORBIDDEN, "P-109", "메인 이벤트(콘서트) 일정은 삭제할 수 없습니다.");
+	SCHEDULE_INVALID_LOCATION_COORDINATES(HttpStatus.BAD_REQUEST, "P-107", "위도와 경도는 함께 제공되어야 합니다."),
+	SCHEDULE_MAIN_EVENT_NOT_DELETABLE(HttpStatus.FORBIDDEN, "P-108", "메인 이벤트(콘서트) 일정은 삭제할 수 없습니다."),
+	USER_NOT_FOUND(HttpStatus.NOT_FOUND, "P-109", "사용자를 찾을 수 없습니다."),
+	USER_ALREADY_PARTICIPANT(HttpStatus.BAD_REQUEST, "P-110", "이미 참가자로 등록된 사용자입니다."),
+	INVALID_SHARE_TOKEN(HttpStatus.NOT_FOUND, "P-111", "유효하지 않은 공유 링크입니다."),
+	SHARE_TOKEN_NOT_GENERATED(HttpStatus.BAD_REQUEST, "P-112", "공유 링크가 생성되지 않았습니다.");
 
 	private final HttpStatus status;
 	private final String code;
