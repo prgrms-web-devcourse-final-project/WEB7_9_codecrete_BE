0b1875d755e75d91818c2853e148cbe5dc5cdfb0
larama-C <ccbj0825@naver.com>
2025-12-29 13:48:53 +0900

    refactor: DTO에 기본 생성자 및 전체 생성자 추가

--- src/main/java/com/back/web7_9_codecrete_be/domain/users/dto/request/UserUpdateNicknameRequest.java
+++ src/main/java/com/back/web7_9_codecrete_be/domain/users/dto/request/UserUpdateNicknameRequest.java
@@ -2,9 +2,13 @@ package com.back.web7_9_codecrete_be.domain.users.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserUpdateNicknameRequest {
 
     @NotBlank(message = "닉네임은 필수입니다.")
