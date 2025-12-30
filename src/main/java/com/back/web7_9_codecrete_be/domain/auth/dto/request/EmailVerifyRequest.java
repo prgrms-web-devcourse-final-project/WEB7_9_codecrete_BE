bc893db947543c9242402f2a739b939974967038
larama-C <ccbj0825@naver.com>
2025-12-30 20:41:33 +0900

    fix: 임시 비밀번호 발급 시 특수문자를 포함하도록 로직 수정

--- src/main/java/com/back/web7_9_codecrete_be/domain/auth/dto/request/EmailVerifyRequest.java
+++ src/main/java/com/back/web7_9_codecrete_be/domain/auth/dto/request/EmailVerifyRequest.java
@@ -1,5 +1,6 @@
 package com.back.web7_9_codecrete_be.domain.auth.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
@@ -12,6 +13,7 @@ public class EmailVerifyRequest {
             regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$",
             message = "이메일 형식이 올바르지 않습니다."
     )
+    @Schema(description = "사용자 이메일", example = "test@example.com")
     private String email;
 
     @NotBlank(message = "인증 코드는 필수입니다.")
