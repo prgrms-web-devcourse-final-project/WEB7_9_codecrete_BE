7cde44b92594a830124651ee3a2d394884cbc9f9
heygeeji <heygeeji@gmail.com>
2026-01-01 13:52:01 +0900

    feat: 티켓팅 사이트 서버 시간

--- src/main/java/com/back/web7_9_codecrete_be/global/security/SecurityConfig.java
+++ src/main/java/com/back/web7_9_codecrete_be/global/security/SecurityConfig.java
@@ -1,7 +1,7 @@
 package com.back.web7_9_codecrete_be.global.security;
 
-import com.back.web7_9_codecrete_be.domain.auth.service.TokenService;
-import lombok.RequiredArgsConstructor;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -14,93 +14,97 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.List;
+import com.back.web7_9_codecrete_be.domain.auth.service.TokenService;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final JwtTokenProvider jwtTokenProvider;
-    private final JwtProperties jwtProperties;
-    private final CustomUserDetailService customUserDetailService;
-    private final TokenService tokenService;
+	private final JwtTokenProvider jwtTokenProvider;
+	private final JwtProperties jwtProperties;
+	private final CustomUserDetailService customUserDetailService;
+	private final TokenService tokenService;
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        http
-                .csrf(csrf -> csrf.disable())
-                .cors(Customizer.withDefaults())
+		http
+			.csrf(csrf -> csrf.disable())
+			.cors(Customizer.withDefaults())
 
-                // 기본 로그인 폼 비활성화
-                .formLogin(form -> form.disable())
+			// 기본 로그인 폼 비활성화
+			.formLogin(form -> form.disable())
 
-                // HTTP Basic 인증 비활성화
-                .httpBasic(basic -> basic.disable())
+			// HTTP Basic 인증 비활성화
+			.httpBasic(basic -> basic.disable())
 
-                // H2 Console 설정
-                .headers(headers -> headers.frameOptions(frame -> frame.disable()))
+			// H2 Console 설정
+			.headers(headers -> headers.frameOptions(frame -> frame.disable()))
 
-                // 세션 관리 설정 - Stateless
-                .sessionManagement((session) -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+			// 세션 관리 설정 - Stateless
+			.sessionManagement((session) -> session
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 
-                // Authorization 설정
+			// Authorization 설정
 
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(org.springframework.http.HttpMethod.OPTIONS, "/**").permitAll()
-                        .requestMatchers(
-                                "/ws-chat/**",
-                                "/actuator/**",
-                                "/api/v1/auth/**",      // 로그인/회원가입은 허용
-                                "/v3/api-docs/**",       // Swagger
-                                "/swagger-ui/**",         // Swagger UI
-                                "/h2-console/**",       // H2 Console
-                                "/api/v1/location/**",      //location 정보 조회 도메인(임시)
-                                "/api/v1/concerts/**",     // concert 정보 조회 도메인
-                                "/api/v1/artists/**",    // artist 정보 저장 도메인(임시)
-                                "/api/v1/users/**",
-                                "/api/v1/chats/**",
-                                "/api/v1/reviews/**",
-                                "api/v1/join/**"
-                        ).permitAll()
+			.authorizeHttpRequests(auth -> auth
+				.requestMatchers(org.springframework.http.HttpMethod.OPTIONS, "/**").permitAll()
+				.requestMatchers(
+					"/ws-chat/**",
+					"/actuator/**",
+					"/api/v1/auth/**",      // 로그인/회원가입은 허용
+					"/v3/api-docs/**",       // Swagger
+					"/swagger-ui/**",         // Swagger UI
+					"/h2-console/**",       // H2 Console
+					"/api/v1/location/**",      //location 정보 조회 도메인(임시)
+					"/api/v1/concerts/**",     // concert 정보 조회 도메인
+					"/api/v1/artists/**",    // artist 정보 저장 도메인(임시)
+					"/api/v1/users/**",
+					"/api/v1/chats/**",
+					"/api/v1/reviews/**",
+					"/api/v1/join/**",
+					"/api/v1/server-time/**"
+				).permitAll()
 
-                        // ADMIN 전용
-                        .requestMatchers("/api/v1/admin/**").hasRole("ADMIN")
+				// ADMIN 전용
+				.requestMatchers("/api/v1/admin/**").hasRole("ADMIN")
 
-                        .anyRequest().authenticated()
-                )
+				.anyRequest().authenticated()
+			)
 
-                .addFilterBefore(
-                        new JwtAuthenticationFilter(jwtTokenProvider, jwtProperties, tokenService),
-                        UsernamePasswordAuthenticationFilter.class
-                );
+			.addFilterBefore(
+				new JwtAuthenticationFilter(jwtTokenProvider, jwtProperties, tokenService),
+				UsernamePasswordAuthenticationFilter.class
+			);
 
-        return http.build();
-    }
+		return http.build();
+	}
 
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
-        return configuration.getAuthenticationManager();
-    }
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+		return configuration.getAuthenticationManager();
+	}
 
-    // CORS 설정(로컬 프론트 통신 허용)
-    @Bean
-    public UrlBasedCorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
+	// CORS 설정(로컬 프론트 통신 허용)
+	@Bean
+	public UrlBasedCorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:8080", "https://www.naeconcertbutakhae.shop"));
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+		configuration.setAllowedOrigins(
+			List.of("http://localhost:3000", "http://localhost:8080", "https://www.naeconcertbutakhae.shop"));
+		configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
 
-        configuration.setAllowedHeaders(List.of("*"));
-        configuration.setExposedHeaders(List.of("Set-Cookie"));
+		configuration.setAllowedHeaders(List.of("*"));
+		configuration.setExposedHeaders(List.of("Set-Cookie"));
 
-        //쿠키 자동으로 넘어가게 설정
-        configuration.setAllowCredentials(true);
+		//쿠키 자동으로 넘어가게 설정
+		configuration.setAllowCredentials(true);
 
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
 
-        return source;
-    }
+		return source;
+	}
 }
